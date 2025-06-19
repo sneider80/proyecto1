eee2f7a98998f885bab5a191f17d2d4feed9df08
sneider80 <sneider80@gmail.com>
2025-06-19 18:43:34 -0500

    Primer cambio

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+{{mensaje}}
+<button type="button" name="cargar" @click="increment">
+  cargar
+</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      mensaje: 0
+    };
+  },
+  methods:{
+    increment(){
+      this.mensaje++;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
